73e2b464e14366ca1d575eccde1e89b339614d6f
Leo Melo <pgp@leomeloxp.dev>
2024-09-29 13:31:53 +0100

    fix: remove await from return

--- supabase/functions/quero-acolher-form-to-email/sendEmail.ts
+++ supabase/functions/quero-acolher-form-to-email/sendEmail.ts
@@ -1,11 +1,11 @@
 export function sendEmail(payload: unknown) {
-  const headers = new Headers( {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${Deno.env.get('RESEND_API_KEY')}`,
+  const headers = new Headers({
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${Deno.env.get("RESEND_API_KEY")}`,
   });
 
-  return await fetch(Deno.env.get('RESEND_API_URL'), {
-    method: 'POST',
+  return fetch(Deno.env.get("RESEND_API_URL"), {
+    method: "POST",
     headers,
     body: JSON.stringify(payload),
   });
