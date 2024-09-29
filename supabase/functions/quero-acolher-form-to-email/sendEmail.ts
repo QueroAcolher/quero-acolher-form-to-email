f9db400e07b99d9f5357555098813f642c102765
Leo Melo <pgp@leomeloxp.dev>
2024-09-29 16:47:02 +0100

    chore: remove unnecessary console logs

--- supabase/functions/quero-acolher-form-to-email/sendEmail.ts
+++ supabase/functions/quero-acolher-form-to-email/sendEmail.ts
@@ -1,10 +1,8 @@
 export async function sendEmail(payload: unknown) {
-  console.log("payload", payload);
   const headers = new Headers({
     "Content-Type": "application/json",
     Authorization: `Bearer ${Deno.env.get("RESEND_API_KEY")}`,
   });
-  console.log("headers", headers);
 
   const result = await fetch(Deno.env.get("RESEND_API_URL"), {
     method: "POST",
