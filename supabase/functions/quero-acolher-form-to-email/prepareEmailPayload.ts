6f1c56d6c662c673086f825541cb2fa505262fc3
Leo Melo <pgp@leomeloxp.dev>
2024-09-29 16:46:29 +0100

    refactor: cleanup and improve formatting of email payload data

--- supabase/functions/quero-acolher-form-to-email/prepareEmailPayload.ts
+++ supabase/functions/quero-acolher-form-to-email/prepareEmailPayload.ts
@@ -1,11 +1,25 @@
 import { type FormSubmissionData } from "./FormSubmissionData.ts";
 
 export function prepareEmailPayload(data: FormSubmissionData) {
-  //
-  const toEmail = data.email;
-  const subject = `New message from ${data.name}`;
+  const toEmail = Deno.env.get("TO_EMAIL");
+  const subject = `New message from ${data.Nome}`;
 
-  const value = Object.entries(data).map(([key, value]) => `${key}: ${value}`)
+  const value = Object.entries(data).filter(([key]) => {
+    return !["redirect_to", "page", "Aceitou Politica de Privacidade"].includes(
+      key,
+    );
+  }).map(([key, value]) => {
+    const label = key;
+    let parsedValue: string;
+    switch (key) {
+      case "Antecedentes Criminais":
+        parsedValue = value === "on" ? "Não" : "Sim";
+        break;
+      default:
+        parsedValue = value.replace(/\n/g, "<br>");
+    }
+    return `<p><strong>${label}</strong>: ${parsedValue}</p>`;
+  })
     .join("\n");
 
   return {
